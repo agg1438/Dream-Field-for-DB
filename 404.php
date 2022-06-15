e7839e77ce177a8e23077b39689cdabfd0dc401e
agg1438 <ayhan@indieoyun.org>
2022-06-15 07:15:06 +0300

    404 eklendi ve bir küçük hata giderildi.

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php @include "header.php" ?>
+
+<title> Dream Field DB Sitesi - 404 Not Found</title>
+
+<div class="container">
+<h3>Bu sayfa yok yada hazırlanacak.</h3>
+<p>Onun yerine <a href="../../../index.php">Anasayfa</a>ya gidiniz.</p>
+</div>
+
+
+<?php @include "footer.php" ?>
--- header.php
+++ header.php
@@ -3,7 +3,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="../css/bootstrap.min.css">
   <link rel="stylesheet" href="../css/bootstrap-grid.min.css">
   <link rel="stylesheet" href="../css/bootstrap-reboot.min.css">
   </head>
